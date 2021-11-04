80c191dffc9d1c23826ee7eab8469080b43032d6
caugustocc <cronuz300@gmail.com>
2021-11-04 08:01:02 -0600

    altaHTTP

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('kui kui')
+//1. importando comudlo http
+import http from 'http';
+
+//2. crear servidor
+//cb (callBAck) es una funcion que se ejecuta ante cualquier peticion de nuestro Server
+//(request, response)
+
+//las tres formas de declarar una funcion
+//Arrow function, funsio declaretion
+const server = http.createServer((req, res)=>{
+    console.log("se ha recibido una peticion")
+    //repuesta
+    res.write("wiiii");
+    //terminamos la conexion
+    res.end();
+});
+//3. poniendo a trabajar el servidor (puerto +3000, ip)
+server.listen(3000, '192.168.1.64',()=> { 
+    console.log("Servidor Escuchando en http://192.168.1.64:300")
+});
+console.log("Holaaaaa Node");
