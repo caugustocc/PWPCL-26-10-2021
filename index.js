f062c80b22ef9ff3e64e0c2289a3d8b93ef56e4a
caugustocc <cronuz300@gmail.com>
2021-11-05 09:47:13 -0600

    S24 Enrutado de peticiones

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ import http from 'http';
 //las tres formas de declarar una funcion
 //Arrow function, funsio declaretion
 const server = http.createServer((req, res)=>{
-    console.log("se ha recibido una peticion")
-    //consola
-    console.log(`URL:  ${req.url}`);
-    console.log(`Rques Method: ${req.method}`)
-    //tipo de contenido
-    res.setHeader("Content-type", "text/html")
-    //repuesta
-    res.write("<html>");
-    res.write("<head><title>My App</title></head>");
-    res.write(`<Body><h1>Mi sitio web 1</h1><br><p>Recurso Solucitdado: ${req.url}</p> </Body>`);
-    res.write("</html>");
-    //terminamos la conexion
-    res.end();
+    let {url, method} = req;
+    console.log(`Se ha solicitado el siguiente recurso: ${method} : ${req}`)
+    //filtrar url
+    if(url==='/'){
+        //retorno tipo HTML
+        res.setHeader("Content-type", "text/html");
+        //respuesta
+        res.write(`<html>`);
+        res.write(`<head><title>My App</title></head>`);
+        res.write(`<Body><h1>Mi sitio web 1</h1><br><p>Recurso Solucitdado: ${req.url}</p> </Body>`);
+        res.write(`</html>`);
+        res.end();
+    }else{
+        res.setHeader("Content-type", "text/html");
+        //respuesta
+        res.write(`<html>`);
+        res.write(`<head><title>My App</title></head>`);
+        res.write(`<Body><h1>Recurso no encontrado</h1></Body>`);
+        res.write(`</html>`);
+        res.end();
+    }
+        
 });
 //3. poniendo a trabajar el servidor (puerto +3000, ip)
 server.listen(3000, '192.168.1.64',()=> { 
