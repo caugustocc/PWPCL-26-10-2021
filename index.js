5974da570e5b4c21f1b8c38d103c95b6192e0d25
caugustocc <cronuz300@gmail.com>
2021-11-08 21:44:54 -0600

    s24DosPeticiones

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//1. importando comudlo http
+//1. importando modulo http
 import http from 'http';
 
 //2. crear servidor
@@ -11,16 +11,41 @@ const server = http.createServer((req, res)=>{
     let {url, method} = req;
     console.log(`Se ha solicitado el siguiente recurso: ${method} : ${req}`)
     //filtrar url
+    //ruta por defecto
     if(url==='/'){
         //retorno tipo HTML
         res.setHeader("Content-type", "text/html");
         //respuesta
         res.write(`<html>`);
         res.write(`<head><title>My App</title></head>`);
-        res.write(`<Body><h1>Mi sitio web 1</h1><br><p>Recurso Solucitdado: ${req.url}</p> </Body>`);
+        res.write(`<Body><h1>Mi sitio web 1</h1><br><p>Recurso Solicitdado: ${req.url}</p> </Body>`);
         res.write(`</html>`);
         res.end();
-    }else{
+    }
+    //ruta de /autor
+    else if(url === "/autor"){
+//s24 enrutando dos recursos
+res.setHeader("Content-type", "text/html");
+//respuesta
+res.write(`<html>`);
+res.write(`<head><title>Autor</title></head>`);
+res.write(`<Body><h1>Carlos Augusto Cruz Cruz</h1><br><p>Recurso Solicitdado: ${req.url}`)
+res.write(`</p><br><img  src="https://avatars.githubusercontent.com/u/92931689?s=400&u=ca25d563312b96bb0597dac5977b81e22fc979f3&v=4" width="250px"> </Body>`);
+res.write(`</html>`);
+res.end();
+    }
+    //ruta /tec informacion de la carrera
+    else if(url === "/tec"){
+        //s24 enrutando dos recursos
+        res.setHeader("Content-type", "text/html");
+        //respuesta
+        res.write(`<html>`);
+        res.write(`<head><title>ITGAM</title></head>`);
+        res.write(`<Body><h1>Instituto Tecnologico Gustavo A. Madero</h1><br><p>Recurso Solicitdado: ${req.url}</p><br><img width="250px" src="https://yosoy.dev/wp-content/uploads/2018/04/itgam.jpg"></Body>`);
+        res.write(`</html>`);
+        res.end();
+    }
+    else{
         res.setHeader("Content-type", "text/html");
         //respuesta
         res.write(`<html>`);
@@ -29,10 +54,11 @@ const server = http.createServer((req, res)=>{
         res.write(`</html>`);
         res.end();
     }
-        
+
 });
 //3. poniendo a trabajar el servidor (puerto +3000, ip)
-server.listen(3000, '192.168.1.64',()=> { 
-    console.log("Servidor Escuchando en http://192.168.1.64:300")
+//a espera de peticiones
+server.listen(3000, '192.168.1.105',()=> { 
+    console.log(`Servidor Escuchando en http://192.168.1.105`)
 });
-console.log("Holaaaaa Node");
+console.log("Enrutamiento de peticiones");
