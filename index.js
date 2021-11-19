7678199534bf145632475e2fda13eb51e2154208
caugustocc <cronuz300@gmail.com>
2021-11-19 09:54:14 -0600

    s29 Instalcion de Express

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //1. importando modulo http
 import http from "http";
+//2. importando modulo de routes
 import routes from "./routes.js";
+//3. Importando express
+import  Express  from "express";
+
+//creae una instancia de Express
+const app = Express(); //
 
 //2. crear servidor
 //cb (callBAck) es una funcion que se ejecuta ante cualquier peticion de nuestro Server
@@ -8,7 +14,7 @@ import routes from "./routes.js";
 
 //las tres formas de declarar una funcion
 //Arrow function, funsio declaretion
-const server = http.createServer(routes.requestHandler);
+const server = http.createServer(app);
 //3. poniendo a trabajar el servidor (puerto +3000, ip)
 //a espera de peticiones
 server.listen(3000, "0.0.0.0", () => {
