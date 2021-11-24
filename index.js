f7b0bd156a35b85dc8fb1e086e6eb4475004a63a
caugustocc <cronuz300@gmail.com>
2021-11-23 20:00:03 -0600

    s30 Administrando Diferentes rutas

--- index.js
+++ index.js
@@ -7,22 +7,17 @@ import  Express  from "express";
 
 //creae una instancia de Express
 const app = Express(); 
-//Registrando el primer middleware
-app.use((req, res, next)=>{
-  //Registrar un mensaje en el log
-  console.log("📍 Estoy en el middleware 1");
-  next()
+//el orden deve de ser del mas especifico al general, pues si es 
+//el "/" (directorio raiz) cae la funciion directa en esta y no 
+//prosigue la busqueda
+app.use(`/about`,(_,res)=>{
+  console.log(`👀 se realizo la peticion: "/about"`)
+  res.send("<h1> Mi APP </h1>\n 🎯 Sitio con NodeJs")
 })
-//registrando el segundo middlewer
-app.use((req, res, next)=> {
-  //Registrar un mensaje en el log
-  console.log("📍 Estoy en el middleware 2");
-  next()
-});
-app.use((req, res)=>{
-  console.log("📍 Estoy en el middleware 3");
+app.use(`/`,(_, res)=>{
+  console.log('📍 Estoy en el middleware 3: "/"');
   console.log("📌Emitiendo respuesta al cliente");
-  res.send(`<h1>MI Respuesta</h1>\n ✨ HOLA`)
+  res.send(`<h1>MI Respuesta</h1>\n ✨ 🎯Bienvenidos al sitio WEB`)
 });
 
 app.listen(3000, `0.0.0.0`,()=>{
