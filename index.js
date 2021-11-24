42f70163f416b3bb55a2af45fdef79b0dcc40f22
caugustocc <cronuz300@gmail.com>
2021-11-23 18:57:48 -0600

    s30 Middlewares con express

--- index.js
+++ index.js
@@ -6,18 +6,26 @@ import routes from "./routes.js";
 import  Express  from "express";
 
 //creae una instancia de Express
-const app = Express(); //
+const app = Express(); 
+//Registrando el primer middleware
+app.use((req, res, next)=>{
+  //Registrar un mensaje en el log
+  console.log("📍 Estoy en el middleware 1");
+  next()
+})
+//registrando el segundo middlewer
+app.use((req, res, next)=> {
+  //Registrar un mensaje en el log
+  console.log("📍 Estoy en el middleware 2");
+  next()
+});
+app.use((req, res)=>{
+  console.log("📍 Estoy en el middleware 3");
+  console.log("📌Emitiendo respuesta al cliente");
+  res.send(`<h1>MI Respuesta</h1>\n ✨ HOLA`)
+});
 
-//2. crear servidor
-//cb (callBAck) es una funcion que se ejecuta ante cualquier peticion de nuestro Server
-//(request, response)
-
-//las tres formas de declarar una funcion
-//Arrow function, funsio declaretion
-const server = http.createServer(app);
-//3. poniendo a trabajar el servidor (puerto +3000, ip)
-//a espera de peticiones
-server.listen(3000, "0.0.0.0", () => {
+app.listen(3000, `0.0.0.0`,()=>{
   console.log(`Servidor Escuchando en http://192.168.1.66`);
 });
-console.log("Enrutamiento de peticiones");
+
