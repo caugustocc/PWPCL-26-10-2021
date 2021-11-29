36285c1a81f73a22e7c342ca03c45e62f93683cc
caugustocc <cronuz300@gmail.com>
2021-11-29 00:06:31 -0600

    s31 Procesando un formulario

--- index.js
+++ index.js
@@ -7,15 +7,53 @@ import  Express  from "express";
 
 //creae una instancia de Express
 const app = Express(); 
+
+//insertando middleware para
+//la lectura de datos desde el cliente
+app.use(Express.urlencoded({extended: false}));
+
 //el orden deve de ser del mas especifico al general, pues si es 
 //el "/" (directorio raiz) cae la funciion directa en esta y no 
 //prosigue la busqueda
+
+//-------ASOCIANDO RUTAS A MIDDLEWARE------
+
+app.use((req,_, next)=>{
+  console.log(`Peticion realizada: "${req.method} : ${req.path}"`);
+  next();
+});
+
 app.use(`/about`,(_,res)=>{
-  console.log(`👀 se realizo la peticion: "/about"`)
+ 
   res.send("<h1> Mi APP </h1>\n 🎯 Sitio con NodeJs")
+});
+
+app.use(`/add-student-form`,(req, res, next)=>{
+  res.send(`
+  <form action="/add-student" method="POST">
+  <label for= "student-name">🤷‍♂️Student Name </label>
+  <input type= "text" name= "name" id="student-name">
+  <button type="submit">✔ Add Student</button>
+  </form>
+  `);
 })
+
+//ruta que procesa el formulario
+app.use('/add-student',(req, res, next)=>{
+  for(const prop in req.body){
+    console.log(`👉 ${prop}: ${req.body[prop]}`);
+  }
+  console.log(`👉 metodo : ${req.method}`);
+
+  res.json(req.body);
+  // //realizando redireccionamiento
+  // res.redirect(`/`);
+});
+
+
+//recurso de formulario
 app.use([`/`,`/home`],(_, res)=>{
-  console.log('📍 Estoy en el middleware 3: "/"');
+ 
   console.log("📌Emitiendo respuesta al cliente");
   res.send(`<h1>MI Respuesta</h1>\n ✨ 🎯Bienvenidos al sitio WEB`)
 });
